d58aefbc2a381db86b2f83fa6295c5c76b694296
samupd2013 <samupd2013@gmail.com>
2021-06-16 16:59:19 +0200

    fix

--- src/functions/updateCartOnChangedProduct/function.ts
+++ src/functions/updateCartOnChangedProduct/function.ts
@@ -3,35 +3,39 @@ import CartRow from '@dbModel/tables/cart';
 import * as AWS from 'aws-sdk';
 
 const updateCartOnChangedProduct = async (productId: string): Promise<void> => {
-    const iterator = await dbContext.scan(CartRow, { filter: { type: 'Equals', subject: 'productId', object: productId } });
+    const paginator = await dbContext.scan(CartRow, { filter: { type: 'Equals', subject: 'productId', object: productId } }).pages();
 
     const cognito = new AWS.CognitoIdentityServiceProvider();
     let userAttributes: AWS.CognitoIdentityServiceProvider.AttributeListType;
     let usersEmail: Array<string>;
 
-    for await (const item of iterator) {
-        item.isChanged = true;
-        await dbContext.update(item, { onMissing: 'skip' });
+    for await (const page of paginator) {
+        for await (const item of page) {
+            item.isChanged = true;
+            await dbContext.update(item, { onMissing: 'skip' });
 
-        userAttributes = await (await cognito.adminGetUser({ Username: item.userId, UserPoolId: process.env.USER_POOL_ID }).promise()).UserAttributes;
-
-        /*
-        userAttributes: [
-            {
-                "Name": "AttributeName",
-                "Value": "AttributeValue"
-            },
-            ...
-        ]
-        */
-
-        userAttributes.forEach(item => {
-            if (item.Name == 'email')
-                usersEmail.push(item.Value);
-        });
-
-
+            userAttributes = await (await cognito.adminGetUser({ Username: item.userId, UserPoolId: process.env.USER_POOL_ID }).promise()).UserAttributes;
+        }
     }
+
+
+    /*
+    userAttributes: [
+        {
+            "Name": "AttributeName",
+            "Value": "AttributeValue"
+        },
+        ...
+    ]
+    */
+
+    userAttributes.forEach(item => {
+        if (item.Name == 'email')
+            usersEmail.push(item.Value);
+    });
+
+
+
     const ses = new AWS.SES();
     const params: AWS.SES.SendEmailRequest = {
         Destination: {
